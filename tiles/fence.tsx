460b0ce10808318664b223def554dcd9f7bd60ae
Colan <cworstellprogramming@gmail.com>
2023-04-22 17:13:53 -0500

    Deletion

--- tiles/fence.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="fence" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="../sprites/Tilesets/tiles/new/fences.png" width="64" height="64"/>
-</tileset>
