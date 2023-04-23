9583de85c87bff2d2d262cff03db373502e425b9
Colan <cworstellprogramming@gmail.com>
2023-04-22 19:30:17 -0500

    Tiles

--- game.py
+++ game.py
@@ -5,13 +5,13 @@ from player import Player
 from game_state import GameState
 import spritesheet
 import gameWorld
-from tiles import *
 
 class Game:
     def __init__(self, screen):
         self.screen = screen
         self.object = []
         self.game_state = GameState.NONE
+        self.map = []
 
     def set_up(self):
         print("do set up")
@@ -20,16 +20,15 @@ class Game:
         self.object.append(player)
         self.game_state = GameState.RUNNING
 
+        self.load_map('maps/map1.txt')
+
     def update(self):
         print("update")
         self.handle_events()
 
-        # gameWorld.createWorld()
+        self.render_map(self.screen)
 
-        # TileMap
-        map = TileMap('sprites/tiles/Map_Tile Layer 1.csv', spritesheet)
-        # Player.x_position,  Player.y_position = map.start_x, map.start_y
-        map.draw_map(self.screen)
+        # gameWorld.createWorld()
 
         for object in self.object:
             object.render(self.screen)
@@ -55,3 +54,35 @@ class Game:
                 if keys[pygame.K_d]:
                     x += movement
                     self.player.update_position(x, y)
+
+    def load_map(self, file_name):
+        with open(file_name) as map_file:
+            for line in map_file:
+                tiles = []
+                for i in range(0, len(line) - 1, 2):
+                    tiles.append(line[i])
+                self.map.append(tiles)
+
+    def render_map(self, screen):
+        y_pos = 0
+        for line in self.map:
+            x_pos = 0
+            for tile in line:
+                image = map_tile_image[tile]
+                rect = pygame.Rect(x_pos * 60, y_pos * 60, 60, 60)
+                screen.blit(image, rect)
+                x_pos += 1
+            y_pos += 1
+
+map_tile_image = {
+    # Non - Collision
+    "G" : pygame.transform.scale(pygame.image.load("sprites/Slices/images/grassHills/grasshills_13.png"), (60, 60)), # Plain Grass
+
+    # Collision
+    "D" : pygame.transform.scale(pygame.image.load("sprites/Slices/images/fence/fences_15.png"), (60, 60)), #Left To Right Fence
+    "Z" : pygame.transform.scale(pygame.image.load("sprites/Slices/images/fence/fences_02.png"), (60, 60)), #Top Left Fence
+    "W" : pygame.transform.scale(pygame.image.load("sprites/Slices/images/fence/fences_04.png"), (60, 60)), #Top Right Fence
+    "X" : pygame.transform.scale(pygame.image.load("sprites/Slices/images/fence/fences_12.png"), (60, 60)), #Bottom Right Fence
+    "Y" : pygame.transform.scale(pygame.image.load("sprites/Slices/images/fence/fences_10.png"), (60, 60)), #Bottom Left Fence
+    "V" : pygame.transform.scale(pygame.image.load("sprites/Slices/images/fence/fences_05.png"), (60, 60)), #Up To Down Fence
+}
--- gameWorld.py
+++ gameWorld.py
@@ -27,12 +27,3 @@ def createWorld():
     GrassSprite = pygame.image.load('sprites/Tilesets/tiles/new/grasshills.png').convert_alpha()
     GrassImg = spritesheet.SpriteSheet(GrassSprite)
     GrassTexture = GrassImg.get_image(16, 16, 0, 80, 4, 4, (0, 0, 0))
-
-    j = 1
-    y = 1
-    for j in range (70):
-        for y in range (30):
-            if j % 3 == 0 and y % 2 == 0 and rn >= 0.3:
-                inGame.blit(GrassTexture, (j * (64 * rn2), y * (64 * rn3)))
-            y += 1
-        j += 1
